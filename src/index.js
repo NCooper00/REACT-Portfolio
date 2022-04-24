b60dc3d552de5fc55265405669844a47612dd405
Nick Cooper <92492572+NCooper00@users.noreply.github.com>
2022-04-23 19:30:07 -0700

    Moving over the portfolio I made a week or two ago into react

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './components/Portfolio';
+
+import './css/reset.css';
+import './css/main.css';
+import './css/pages.css';
+import './css/skills.css';
+import './css/projects.css';
+import './css/contact.css';
+
+ReactDOM.render(
+
+    <Portfolio />,
+
+  document.querySelector('body')
+);
