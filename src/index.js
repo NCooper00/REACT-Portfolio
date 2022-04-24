d6271ad1d25cd81cf4005bef77c7a732bf3b89cb
Nick Cooper <92492572+NCooper00@users.noreply.github.com>
2022-04-23 20:06:01 -0700

    Added some stuff I think

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Portfolio from './components/Portfolio';
 
-import './css/reset.css';
-import './css/main.css';
-import './css/pages.css';
-import './css/skills.css';
-import './css/projects.css';
-import './css/contact.css';
+// import './css/reset.css';
+// import './css/main.css';
+// import './css/pages.css';
+// import './css/skills.css';
+// import './css/projects.css';
+// import './css/contact.css';
 
 ReactDOM.render(
 
